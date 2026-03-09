5666b48359ba3cb46bba687b44132f0dfb2b8e7e
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2026-03-09 23:34:44 +0300

    add file

---
+++ src/Pretty_printer/tests_for_pretty_printer.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    // Тест 1
+    printf("Тест 1: a;b и c;d\n");
+    FILE* f = fopen("input.csv", "w");
+    fprintf(f, "a;b\n");
+    fprintf(f, "c;d\n");
+    fclose(f);
+    
+    system("pretty_printer.exe");
+    ;
+    f = fopen("output.txt", "r");
+    if (f) {
+        char c;
+        while ((c = fgetc(f)) != EOF) {
+            putchar(c);
+        }
+        fclose(f);
+    }
+    printf("\n");
+    
+    // Тест 2
+    f = fopen("input.csv", "w");
+    fprintf(f, "hello;world\n");
+    fclose(f);
+    
+    system("pretty_printer.exe");
+    
+    f = fopen("output.txt", "r");
+    if (f) {
+        char c;
+        while ((c = fgetc(f)) != EOF) {
+            putchar(c);
+        }
+        fclose(f);
+    }
+    printf("\n");
+    
+    // Тест 3
+    f = fopen("input.csv", "w");
+    fprintf(f, "1;2;3\n");
+    fprintf(f, "4;5;6\n");
+    fprintf(f, "7;8;9\n");
+    fclose(f);
+    
+    system("pretty_printer.exe");
+
+    f = fopen("output.txt", "r");
+    if (f) {
+        char c;
+        while ((c = fgetc(f)) != EOF) {
+            putchar(c);
+        }
+        fclose(f);
+    }
+    printf("\n");
+    
+    return 0;
+}
