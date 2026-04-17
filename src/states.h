a51503df08dda5912b66857b654f83afb88c1288
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2026-04-17 22:02:35 +0300

    Add files via upload

--- src/states.h
+++ src/states.h
@@ -1,29 +1,26 @@
 #pragma once
 #include <stdio.h>
 
+typedef struct {
+    int distance;
+    int city;
+    int state;
+} QueueElement;
+
+typedef struct {
+    QueueElement* data;
+    int size;
+    int capacity;
+} Queue;
+
 typedef struct Edge {
     int to;
     int length;
     struct Edge* next;
 } Edge;
 
-typedef struct QueueElement {
-    int distance;
-    int city;
-    int state;
-} QueueElement;
-
-typedef struct Queue {
-    QueueElement* data;
-    int size;
-    int capacity;
-} Queue;
-
 void initQueue(Queue* pq, int capacity);
 void freeQueue(Queue* pq);
-void swap(QueueElement* a, QueueElement* b);
-void heapifyUp(Queue* pq, int index);
-void heapifyDown(Queue* pq, int index);
 void push(Queue* pq, int distance, int city, int state);
 QueueElement pop(Queue* pq);
 int isEmpty(Queue* pq);
