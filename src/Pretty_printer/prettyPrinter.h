eb320578f3ba9e6eb4651e3fcdccbb8733deabe2
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2026-04-23 21:50:22 +0300

    Update prettyPrinter.h

--- src/Pretty_printer/prettyPrinter.h
+++ src/Pretty_printer/prettyPrinter.h
@@ -1,17 +1,16 @@
-#pragma once
+#ifndef PRETTY_PRINTER_H
+#define PRINTY_PRINTER_H
+
 #include <stdio.h>
 
 typedef struct {
-    int numCols;
-    int *colWidths;
+  int numCols;
+  int *colWidths;
 } TableConfig;
 
-int is_number(const char *str);
-
-TableConfig* createTableConfig(FILE *file, char delimiter);
-
+TableConfig *createTableConfig(FILE *file, char delimiter);
 void freeTableConfig(TableConfig *config);
-
 void printSeparatorLine(FILE *output, TableConfig *config);
-
 void printRow(FILE *output, char *line, TableConfig *config, char delimiter);
+
+#endif
