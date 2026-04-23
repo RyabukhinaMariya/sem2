42e611cc9e554de38db9163f744194047257a291
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2026-04-23 21:52:50 +0300

    fix ci issues

--- src/Pretty_printer/createTable.c
+++ src/Pretty_printer/createTable.c
@@ -4,20 +4,23 @@
 #include <string.h>
 
 int isNumber(const char *str) {
-  if (!str || *str == '\0') return 0;
+  if (!str || *str == '\0')
+    return 0;
 
   int dotCount = 0;
   int i = 0;
 
   if (str[0] == '-') {
-    if (strlen(str) == 1) return 0;
+    if (strlen(str) == 1)
+      return 0;
     i = 1;
   }
 
   for (; str[i] != '\0'; i++) {
     if (str[i] == '.') {
       dotCount++;
-      if (dotCount > 1) return 0;
+      if (dotCount > 1)
+        return 0;
     } else if (!isdigit(str[i])) {
       return 0;
     }
@@ -27,14 +30,16 @@ int isNumber(const char *str) {
 
 TableConfig *createTableConfig(FILE *file, char delimiter) {
   TableConfig *config = malloc(sizeof(TableConfig));
-  if (!config) return NULL;
+  if (!config)
+    return NULL;
 
   int ch, currentLen = 0, col = 0, maxCols = 1;
 
   // find count of columns
   rewind(file);
   while ((ch = fgetc(file)) != '\n' && ch != EOF) {
-    if (ch == delimiter) maxCols++;
+    if (ch == delimiter)
+      maxCols++;
   }
 
   config->numCols = maxCols;
@@ -66,7 +71,8 @@ void freeTableConfig(TableConfig *config) {
 void printSeparatorLine(FILE *output, TableConfig *config) {
   fprintf(output, "+");
   for (int i = 0; i < config->numCols; i++) {
-    for (int j = 0; j < config->colWidths[i] + 2; j++) fprintf(output, "-");
+    for (int j = 0; j < config->colWidths[i] + 2; j++)
+      fprintf(output, "-");
     fprintf(output, "+");
   }
   fprintf(output, "\n");
@@ -94,7 +100,8 @@ void printRow(FILE *output, char *line, TableConfig *config, char delimiter) {
             isNum ? config->colWidths[col] : -config->colWidths[col], buffer);
     fprintf(output, "|");
 
-    if (*rest == delimiter) rest++;
+    if (*rest == delimiter)
+      rest++;
     col++;
   }
   fprintf(output, "\n");
