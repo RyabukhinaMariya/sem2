14ceacbf0974305bc85dcd4005f5b55596e6ed98
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2026-04-23 21:49:06 +0300

    Update main.c

--- src/Pretty_printer/main.c
+++ src/Pretty_printer/main.c
@@ -1,42 +1,44 @@
+#include "prettyPrinter.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "prettyPrinter.h"
 
-#define MAX_LINE_SIZE 2048
-#define DELIMITER ';'
+#define DELIMITER ','
+#define MAX_LINE_SIZE 1024
 
 int main() {
-    FILE *input = fopen("input.csv", "r");
-    FILE *output = fopen("output.txt", "w");
+  FILE *input = fopen("input.csv", "r");
+  FILE *output = fopen("output.txt", "w");
 
-    if (!input || !output) {
-        perror("cannot open the files");
-        if (input) fclose(input);
-        return 1;
-    }
+  if (!input || !output) {
+    perror("cannot open the files");
+    if (input) fclose(input);
+    if (output) fclose(output);
+    return 1;
+  }
 
-    TableConfig *config = createTableConfig(input, DELIMITER);
-    if (!config) {
-        fclose(input); fclose(output);
-        return 1;
-    }
-
-    rewind(input);
-    char line[MAX_LINE_SIZE];
-
-    printSeparatorLine(output, config);
-    while (fgets(line, sizeof(line), input)) {
-        if (strlen(line) <= 1) continue;
-        
-        printRow(output, line, config, DELIMITER);
-        printSeparatorLine(output, config);
-    }
-
-    freeTableConfig(config);
+  TableConfig *config = createTableConfig(input, DELIMITER);
+  if (!config) {
     fclose(input);
     fclose(output);
+    return 1;
+  }
 
-    printf("table is succesfully writen to output.txt\n");
-    return 0;
+  rewind(input);
+  char line[MAX_LINE_SIZE];
+
+  printSeparatorLine(output, config);
+  while (fgets(line, sizeof(line), input)) {
+    if (strlen(line) <= 1) continue;
+
+    printRow(output, line, config, DELIMITER);
+    printSeparatorLine(output, config);
+  }
+
+  freeTableConfig(config);
+  fclose(input);
+  fclose(output);
+
+  printf("table is succesfully writen to output.txt\n");
+  return 0;
 }
