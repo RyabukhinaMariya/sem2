b46b8bd90824e577d9be38fc1a11369b2427313c
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2026-04-23 22:53:51 +0300

    fix ci issues

--- src/Airports/tests.c
+++ src/Airports/tests.c
@@ -1,68 +1,69 @@
 #include "airports.h"
+#include <assert.h>
 #include <stdio.h>
 
 void testInsertAndFind(void) {
-    printf("Test insert and find\n");
-    AVLTree* tree = createAVL();
+  printf("Test insert and find\n");
+  AVLTree *tree = createAVL();
 
-    tree = avlInsert(tree, "KRR", "Pashkovsky");
+  tree = avlInsert(tree, "KRR", "Pashkovsky");
 
-    assert(strcmp(find(tree, "KRR"), "Pashkovsky") == 0);
-    assert(find(tree, "ZZZ") == NULL);
+  assert(strcmp(find(tree, "KRR"), "Pashkovsky") == 0);
+  assert(find(tree, "ZZZ") == NULL);
 
-    freeAVL(tree);
+  freeAVL(tree);
 }
 
 void testDelete(void) {
-    printf("Test Simple Delete\n");
-    AVLTree* tree = createAVL();
+  printf("Test Simple Delete\n");
+  AVLTree *tree = createAVL();
 
-    tree = avlInsert(tree, "KRR", "Pashkovsky");
-    tree = avlInsert(tree, "QQQ", "Airport2");
-    tree = avlInsert(tree, "WWW", "Airport3");
+  tree = avlInsert(tree, "KRR", "Pashkovsky");
+  tree = avlInsert(tree, "QQQ", "Airport2");
+  tree = avlInsert(tree, "WWW", "Airport3");
 
-    tree = avlDeleteNode(tree, "KRR");
-    assert(find(tree, "KRR") == NULL);
-    assert(find(tree, "AAA") != NULL);
-    assert(find(tree, "BBB") != NULL);
+  tree = avlDeleteNode(tree, "KRR");
+  assert(find(tree, "KRR") == NULL);
+  assert(find(tree, "AAA") != NULL);
+  assert(find(tree, "BBB") != NULL);
 
-    freeAVL(tree);
+  freeAVL(tree);
 }
 
 void testGeneral(void) {
-    printf("Test Interface\n");
-    char* testFile = "airports.txt";
+  printf("Test Interface\n");
+  char *testFile = "airports.txt";
 
-    FILE* open = fopen(testFile, "w");
-    fprintf(open, "KRR:Pashkovsky\n");
-    fprintf(open, "AAA:Airport1\n");
-    fclose(open);
+  FILE *open = fopen(testFile, "w");
+  fprintf(open, "KRR:Pashkovsky\n");
+  fprintf(open, "AAA:Airport1\n");
+  fclose(open);
 
-    AVLTree* tree = load(testFile);
-    assert(tree != NULL);
-    assert(strcmp(find(tree, "KRR"), "Pashkovsky") == 0);
-    assert(strcmp(find(tree, "AAA"), "Airport1") == 0);
+  AVLTree *tree = load(testFile);
+  assert(tree != NULL);
+  assert(strcmp(find(tree, "KRR"), "Pashkovsky") == 0);
+  assert(strcmp(find(tree, "AAA"), "Airport1") == 0);
 
-    tree = add(tree, "BBB:Aiport2");
-    assert(strcmp(find(tree, "BBB"), "Airport2") == 0);
+  tree = add(tree, "BBB:Aiport2");
+  assert(strcmp(find(tree, "BBB"), "Airport2") == 0);
 
-    char* outputFile = "testOutput.txt";
-    saveToFile(tree, outputFile);
-    freeAVL(tree);
+  char *outputFile = "testOutput.txt";
+  saveToFile(tree, outputFile);
+  freeAVL(tree);
 
-    AVLTree* tree2 = load(outputFile);
-    assert(tree2 != NULL);
-    assert(find(tree2, "LED") != NULL);
-    assert(find(tree2, "AER") != NULL);
+  AVLTree *tree2 = load(outputFile);
+  assert(tree2 != NULL);
+  assert(find(tree2, "LED") != NULL);
+  assert(find(tree2, "AER") != NULL);
 
-    freeAVL(tree2);
-    remove(testFile);
-    remove(outputFile);
+  freeAVL(tree2);
+  remove(testFile);
+  remove(outputFile);
 }
 
 void runTests(void) {
-    testInsertAndFind();
-    testDelete();
-    testGeneral();
-    printf("All tests passed!\n");
+  testInsertAndFind();
+  testDelete();
+  testGeneral();
+  printf("All tests passed!\n");
 }
