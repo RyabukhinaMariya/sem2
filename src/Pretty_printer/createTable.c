d5ede8f82ae4738e61a99c603c3f36b796584068
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2026-04-23 21:47:00 +0300

    fix ci issues

--- src/Pretty_printer/createTable.c
+++ src/Pretty_printer/createTable.c
@@ -2,32 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-int isNumber(const char *str){
+int isNumber(const char *str) {
   if (!str || *str == '\0')
     return 0;
 
   int dotCount = 0;
   int i = 0;
 
-  if (str[0] == '-'){
+  if (str[0] == '-') {
     if (strlen(str) == 1)
       return 0;
     i = 1;
   }
 
-  for (; str[i] != '\0'; i++){
+  for (; str[i] != '\0'; i++) {
     if (str[i] == '.') {
       dotCount++;
       if (dotCount > 1)
         return 0;
-    } else if (!isdigit(str[i])){
+    } else if (!isdigit(str[i])) {
       return 0;
     }
   }
   return 1;
 }
 
-TableConfig *createTableConfig(FILE *file, char delimiter){
+TableConfig *createTableConfig(FILE *file, char delimiter) {
   TableConfig *config = malloc(sizeof(TableConfig));
   if (!config)
     return NULL;
@@ -36,7 +36,7 @@ TableConfig *createTableConfig(FILE *file, char delimiter){
 
   // find count of columns
   rewind(file);
-  while ((ch = fgetc(file)) != '\n' && ch != EOF){
+  while ((ch = fgetc(file)) != '\n' && ch != EOF) {
     if (ch == delimiter)
       maxCols++;
   }
@@ -48,7 +48,7 @@ TableConfig *createTableConfig(FILE *file, char delimiter){
   col = 0;
   while ((ch = fgetc(file)) != EOF) {
     if (ch == delimiter || ch == '\n') {
-      if (currentLen > config->colWidths[col]){
+      if (currentLen > config->colWidths[col]) {
         config->colWidths[col] = currentLen;
       }
       currentLen = 0;
@@ -60,16 +60,16 @@ TableConfig *createTableConfig(FILE *file, char delimiter){
   return config;
 }
 
-void freeTableConfig(TableConfig *config){
-  if (config){
+void freeTableConfig(TableConfig *config) {
+  if (config) {
     free(config->colWidths);
     free(config);
   }
 }
 
-void printSeparatorLine(FILE *output, TableConfig *config){
+void printSeparatorLine(FILE *output, TableConfig *config) {
   fprintf(output, "+");
-  for (int i = 0; i < config->numCols; i++){
+  for (int i = 0; i < config->numCols; i++) {
     for (int j = 0; j < config->colWidths[i] + 2; j++)
       fprintf(output, "-");
     fprintf(output, "+");
@@ -77,14 +77,14 @@ void printSeparatorLine(FILE *output, TableConfig *config){
   fprintf(output, "\n");
 }
 
-void printRow(FILE *output, char *line, TableConfig *config, char delimiter){
+void printRow(FILE *output, char *line, TableConfig *config, char delimiter) {
   char *token;
   char *rest = line;
   int col = 0;
 
   fprintf(output, "|");
 
-  while (col < config->numCols){
+  while (col < config->numCols) {
     char buffer[256] = {0};
     int i = 0;
 
