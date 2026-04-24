f47c886122fea3685a7cbfb764bfe1750c0ab61c
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2026-04-24 08:47:47 +0300

    fix ci issues

--- src/states.h
+++ src/states.h
@@ -8,7 +8,7 @@ typedef struct {
 } QueueElement;
 
 typedef struct {
-    QueueElement* data;
+    QueueElement *data;
     int size;
     int capacity;
 } Queue;
@@ -16,11 +16,11 @@ typedef struct {
 typedef struct Edge {
     int to;
     int length;
-    struct Edge* next;
+    struct Edge *next;
 } Edge;
 
-void initQueue(Queue* pq, int capacity);
-void freeQueue(Queue* pq);
-void push(Queue* pq, int distance, int city, int state);
-QueueElement pop(Queue* pq);
-int isEmpty(Queue* pq);
+void initQueue(Queue *pq, int capacity);
+void freeQueue(Queue *pq);
+void push(Queue *pq, int distance, int city, int state);
+QueueElement pop(Queue *pq);
+int isEmpty(Queue *pq);
